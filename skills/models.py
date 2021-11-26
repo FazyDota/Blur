41d9bb511856e4fcb436a5abe2f9240f4159f92b
FazyGames <Fazy64@gmail.com>
2021-11-26 10:10:41 +0100

    Another fix to the model Ability

--- skills/models.py
+++ skills/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Ability(models.Model):
-    name = models.CharField()
-    hero = models.CharField()
+    name = models.CharField(max_length=60)
+    hero = models.CharField(max_length=60)
     agi_winrate = models.FloatField(null=True)
     agi_pickorder = models.FloatField(null=True)
     int_winrate = models.FloatField(null=True)
@@ -15,4 +15,4 @@ class Ability(models.Model):
     melee_pickorder = models.FloatField(null=True)
     ranged_winrate = models.FloatField(null=True)
     ranged_pickorder = models.FloatField(null=True)
-    image = models.CharField(null=True)
+    image = models.CharField(max_length=60, null=True)
